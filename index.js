07d127895e50ef9df9e21d515254cebf0872b3fa
alexandrabuica <alexandrabuica99@gmail.com>
2022-05-10 23:53:32 +0300

    Second backend setup

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require("body-parser");
+
+const searchesRouter = require('./router/searchesRouter');
 
 const app = express();
+
 app.use(cors());
 
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/searches", searchesRouter);
+
 const port = process.env.PORT || 8080;
+
 
 app.listen(port, () => {
     console.log(`Cloud computing app listening on port ${port}!`)
