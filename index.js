783db189249ea9a9fd37a8ce4ab02fd2a62e8ad9
alexandrabuica <alexandrabuica99@gmail.com>
2022-05-10 21:37:42 +0300

    Initial backend setup

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`Cloud computing app listening on port ${port}!`)
+});
